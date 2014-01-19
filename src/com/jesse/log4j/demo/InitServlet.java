27b1e5eae03885e73e8a978bb71f0fbd4cb2c436
mupengfei <mupf.zj@gmail.com>
2014-01-20 00:51:21 +0800

    hghg

---
+++ src/com/jesse/log4j/demo/InitServlet.java
@@ -0,0 +1,32 @@
+package com.jesse.log4j.demo;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class InitServlet extends HttpServlet {
+	private Logger logger = LogManager.getLogger(InitServlet.class.getName());  
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+		System.out.println("Hello World");
+		logger.error("Hello World");
+		logger.info("Hello World");
+	}
+
+}
